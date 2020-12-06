17769916b1bcfd8fda1d92beda77b7caca4baf9a
jhong40 <jhong40@gmail.com>
2020-12-06 15:21:32 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,31 +2,31 @@ import sun.misc.Signal;
 
 public class Main {
 
-	    public static void main(String[] args) throws Exception {
-		            handleSignal("TERM");
+    public static void main(String[] args) throws Exception {
+        handleSignal("TERM");
 
-			            while (true) {
-					                try {
-								                Thread.sleep(1000);
-										                System.out.println("I'm doing some work ...");
-												            } catch (InterruptedException ex) {
-														                    Thread.currentThread().interrupt();
-																                }
-							        }
-				        }
-	        
-	        private static void shutdown() {
-			        System.out.println("Shutdown initiated");
-				        System.exit(0);
-					    }
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("I'm doing some work ...");
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 
-		    private static void handleSignal(String name) {
-			            Signal signal = new Signal(name);
-				            Signal.handle(signal, Signal -> {
-						                System.out.println("Signal received: " + signal.getName());
-								            if ("TERM".equals(signal.getName())) {
-										                    shutdown();
-												                }
-									            });
-					        }
+    private static void shutdown() {
+        System.out.println("Shutdown initiated");
+        System.exit(0);
+    }
+
+    private static void handleSignal(String name) {
+        Signal signal = new Signal(name);
+        Signal.handle(signal, Signal - > {
+            System.out.println("Signal received: " + signal.getName());
+            if ("TERM".equals(signal.getName())) {
+                shutdown();
+            }
+        });
+    }
 }
